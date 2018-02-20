5d07d491a148267df7eede99a91f9a743387b9c0
coletriebold <35664168+coletriebold@users.noreply.github.com>
2018-02-19 17:15:51 -0800

    Update water-arrays.js

--- water-arrays.js
+++ water-arrays.js
@@ -1,14 +1,20 @@
 var x = [];
 var y = [];
+var xSpeed = [];
+var ySpeed = [];
 
 function setup() {
   createCanvas(400, 400);
   colorMode(HSB)
 	x[0] = 220;
 	y[0] = 200;
+	xSpeed[0] = 0;
+	ySpeed[0] = 3;
 	for (var i = 1; i < 10; i += 1) {
 		x[i] = 220+random(-10,10);
 		y[i]= y[i-1] +random(10,25);
+		xSpeed[i] = random(-1,1);
+		ySpeed[i] = random(2,4);
 	}
 }
 
@@ -20,7 +26,8 @@ function draw() {
   
 	for (var i = 0; i < 10; i += 1) {
 		ellipse(x[i],y[i],10);
-		y[i]= y[i] + 3;
+		y[i]= y[i] + ySpeed[i];
+		x[i] = x[i]+xSpeed[i]
 	}
 	if (y[9]>height) {
 		for (var j=9;j>0;j-=1) {
